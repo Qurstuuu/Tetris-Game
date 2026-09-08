b45e61b90b67dc72ac6ba45c22b5a2727f37254b
Qurstuuu <64999056+Qurstuuu@users.noreply.github.com>
2026-09-08 19:01:21 +0700

    загрузила исходника

---
+++ Tetris/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Tetris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelHighScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.Location = new System.Drawing.Point(12, 12);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(75, 23);
+            this.buttonRestart.TabIndex = 0;
+            this.buttonRestart.TabStop = false;
+            this.buttonRestart.Text = "Restart";
+            this.buttonRestart.UseVisualStyleBackColor = true;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelScore.ForeColor = System.Drawing.Color.Red;
+            this.labelScore.Location = new System.Drawing.Point(113, 15);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(104, 20);
+            this.labelScore.TabIndex = 1;
+            this.labelScore.Text = "Game Over!";
+            // 
+            // labelHighScore
+            // 
+            this.labelHighScore.AutoSize = true;
+            this.labelHighScore.Location = new System.Drawing.Point(237, 20);
+            this.labelHighScore.Name = "labelHighScore";
+            this.labelHighScore.Size = new System.Drawing.Size(87, 13);
+            this.labelHighScore.TabIndex = 2;
+            this.labelHighScore.Text = "No highscore yet";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(450, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Next Piece:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 761);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelHighScore);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.buttonRestart);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Tetris!";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnKeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRestart;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelHighScore;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Tetris/OPiece.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal class OPiece : Tetromino
+    {
+        private static readonly int[,] SHAPE = { { 8, 8 }, { 8, 8 } };
+        private static readonly int[,] BOOM_SHAPE =
+        {
+            { 0, -1, -1, -1, -1, 0 },
+            {-1, -1, -1, -1, -1, -1 },
+            {-1, -1, -1, -1, -1, -1 },
+            {-1, -1, -1, -1, -1, -1 },
+            {-1, -1, -1, -1, -1, -1 },
+            { 0, -1, -1, -1, -1, 0 }
+        };
+
+        public OPiece(int x = 0, int y = 21) :
+            base(SHAPE, x, y)
+        {
+
+        }
+
+        public OPiece(int[,] shape, int x = 0, int y = 21) :
+            base(shape, x, y)
+        {
+
+        }
+
+        public OPiece(OPiece other) : base(other._shape, other._x, other._y) // конструктор копирования
+        {
+
+        }
+
+        public override Tetromino Clone()
+        {
+            return new OPiece(this);
+        }
+
+        public override Tetromino Move(int dx, int dy) // мерцает черным и красным при движении
+        {
+            int changeTo = 7;
+            if (_shape[0, 0] == 7) changeTo = 8;
+            int[,] newShape = new int[Size, Size];
+            for (int i = 0; i < Size; i++)
+                for (int j = 0; j < Size; j++)
+                    newShape[i, j] = changeTo;
+
+            return new OPiece(newShape, _x + dx, _y + dy);
+        }
+
+        public override Tetromino Rotate() // ничего не меняется при повороте т.к. это квадрат
+        {
+            return this;
+        }
+
+        public override Tetromino SetCoords(int x, int y)
+        {
+            return new OPiece(_shape, x, y);
+        }
+
+        public Tetromino Explosion()
+        {
+            int centerX = _x + Size / 2;
+            int centerY = _y + Size / 2;
+            int boomX = centerX - 3;
+            int boomY = centerY - 3;
+
+            return new OPiece(BOOM_SHAPE, boomX, boomY);
+        }
+    }
+}
